24098e0fd93136b98950edb477933c5a6f268064
eccos <eccos@users.noreply.github.com>
2024-01-08 21:11:37 -0800

    remove replies field

--- models/comment.js
+++ models/comment.js
@@ -3,10 +3,9 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema(
   {
-    text: { type: String, required: true },
-    // author: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to User model
     parent: { type: Schema.Types.ObjectId, ref: 'Comment', default: null }, // Reference to the parent comment
-    replies: [{ type: Schema.Types.ObjectId, ref: 'Comment' }], // Self-referencing Comment schema
+    // author: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to User model
+    text: { type: String, required: true },
     deleted: { type: Boolean },
   },
   { timestamps: true }
