62973f3534b3b6d1caaf16d89c7d0bd92300d545
eccos <eccos@users.noreply.github.com>
2024-01-09 01:28:31 -0800

    re-enable file serving to fix content-type issue & add charset

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 const hostname = 'localhost';
 const serverPort = 3000;
-const serverUrl = `http://${hostname}:${serverPort}/`;
+const serverUrl = `http://${hostname}:${serverPort}`;
 const dbPort = 27017;
 const db = 'commentsApp';
 const dbUrl = `mongodb://${hostname}:${dbPort}/${db}`;
@@ -25,14 +25,17 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 // allows access to /public folder if path and file ext are known
-// removed b/c i want routers to handle it
-// app.use(express.static(__dirname + '/public'));
+// removed b/c i want routers to handle it.
+// re-added b/c it's needed for express to serve files w/ the correct content-type
+// w/o it, express serves an html file correctly as 'text/html', but...
+// the js file it links is also served as 'text/html' instead of 'text/javascript'
+app.use(express.static(__dirname + '/public'));
 
 app.use('/comments', commentRouter);
 
 app.use((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end('This is an Express Server');
 });
 
