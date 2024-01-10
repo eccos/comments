d2726875e7d0535a019c50700a47d54e93f5050d
eccos <eccos@users.noreply.github.com>
2024-01-10 02:54:07 -0800

    refactor using async/await

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,6 +1,6 @@
 import { baseUrl } from '../shared/baseUrl.js';
-const commentsApi = 'comments';
-const commentsUrl = `${baseUrl}/${commentsApi}`;
+const commentsApi = location.pathname; // /comments
+const commentsUrl = baseUrl + commentsApi;
 
 const commentsElem = document.getElementById('comments');
 
@@ -11,26 +11,25 @@ const commentsElem = document.getElementById('comments');
 // 2. after posting comment, redirect user to posted comment
 getComments(); // get comments and append to screen
 
-function getComments() {
-  fetch(commentsUrl, {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-    },
-  })
-    .then(async (res) => {
-      if (res.ok) {
-        const comments = await res.json();
-        commentsElem.textContent = '';
-        updateDOM(comments);
-      } else {
-        commentsElem.textContent = 'No comments found';
-      }
-    })
-    .catch((err) => {
-      // TODO: run request again depending on error
-      commentsElem.textContent = err;
+async function getComments() {
+  try {
+    const res = await fetch(commentsUrl, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
     });
+    if (res.ok) {
+      const comments = await res.json();
+      commentsElem.textContent = '';
+      updateDOM(comments);
+    } else {
+      commentsElem.textContent = 'No comments found';
+    }
+  } catch (err) {
+    // TODO: run request again depending on error
+    commentsElem.textContent = err;
+  }
 
   function updateDOM(comments) {
     comments.forEach((comment) => {
@@ -44,19 +43,19 @@ function getComments() {
         border: '1px solid black',
       };
       Object.assign(container.style, styleObj);
+      linkWrapper.appendChild(container);
 
       const commentElem = document.createElement('div');
       commentElem.textContent = comment.text;
-
       container.appendChild(commentElem);
-      linkWrapper.appendChild(container);
+
       commentsElem.appendChild(linkWrapper);
     });
   }
 }
 
 document.getElementById('postBtn').addEventListener('click', postComment);
-function postComment() {
+async function postComment() {
   const commentText = document.getElementById('commentText');
   const postStatus = document.getElementById('postStatus');
 
@@ -71,18 +70,17 @@ function postComment() {
 
   const comment = { text };
 
-  fetch(commentsUrl, {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(comment),
-  })
-    .then((res) => {
-      postStatus.innerHTML = `<strong>Server Status Code:</strong> ${res.status} <br><strong>Server Status Text:</strong> ${res.statusText}`;
-    })
-    .catch((err) => {
-      postStatus.textContent = err;
+  try {
+    const res = await fetch(commentsUrl, {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(comment),
     });
+    postStatus.innerHTML = `<strong>Server Status Code:</strong> ${res.status} <br><strong>Server Status Text:</strong> ${res.statusText}`;
+  } catch (err) {
+    postStatus.textContent = err;
+  }
 }
